b5503686a0d59bbff7b89e29497cfba04c44d641
tiagomarcoss <tiagomarcosp@gmail.com>
2024-08-24 11:06:26 -0300

    Padronização de nomes dos templates
    Para melhor leitura, os templates sempre ficarão com o nome esse formato: {nome_do_app}_{função}-{objeto_que_se_refere}
    
    ex: banco_c-cotacao
    banco (app), c (de create), cotacao(objeto)
    
    os nomes já foram alterados também nos 'returns' de suas respectivas funções

--- banco/urls.py
+++ banco/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('c-cotacao/', create_cotacao, name='create_cotacao'),
-    path('g-cotacao/', get_cotacao, name='get_cotacao')
+    path('g-cotacao/', get_cotacao, name='get_cotacao'),
+    path('c-conta_bancaria/',create_conta_bancaria, name='create_conta_bancaria' ),
 ]

aacabcf8b2182402f59f6d47b5333f7384b9a810
tiagomarcoss <tiagomarcosp@gmail.com>
2024-08-24 09:36:38 -0300

    Modificação views.py / Guardando dados inseridos
    A função create_cotacao é responsável por renderizar a pagina e conseguir guardar os dados no banco caso o usuário envie um request no formato POST, verifica se é valido e após isso salva.

--- banco/views.py
+++ banco/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
+from .forms import *
+import requests
 # Create your views here.
+
+def create_cotacao(request):
+    form = CotacaoForm()
+    if request.method == 'POST':  
+        form = CotacaoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+    
+    return render(request, 'banco.html', {'form':form})

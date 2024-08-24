464e606cda85385dce4d7d6cfbf75d0c9989cfb5
tiagomarcoss <tiagomarcosp@gmail.com>
2024-08-24 09:24:36 -0300

    Modificação models: Novo objeto Cotacao
    Nesse esse objeto servirá como vao funcionar o banco de dados, pegando dados de uma API sobre cotação de moedas.

--- banco/models.py
+++ banco/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Cotacao(models.Model):
+    moeda_1 = models.CharField(max_length=3)
+    moeda_2 = models.CharField(max_length=3)
+    conversao = models.CharField(max_length=100, blank=True, null=True)
+    valor_venda = models.FloatField(blank=True, null=True)
+    valor_compra = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.conversao}'

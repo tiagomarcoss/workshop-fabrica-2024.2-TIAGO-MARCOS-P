e3511071f934ab545def028d4fc523cb92bc8222
tiagomarcoss <tiagomarcosp@gmail.com>
2024-08-24 10:47:58 -0300

    Criacão de forms para o Obejto conta bancária
    Formulario input criado de acordo com o modelo ContaBancária

--- banco/forms.py
+++ banco/forms.py
@@ -5,3 +5,15 @@ class CotacaoForm(forms.ModelForm):
     class Meta:
         model = Cotacao
         fields = ['moeda_1','moeda_2'] 
+
+class ContaBancariaForm(forms.ModelForm):
+    class Meta:
+        model = ContaBancaria
+        fields = [
+    'saldo_BLR', 
+    'saldo_USD',
+    'saldo_EUR',
+    'saldo_BTC',
+    'salgo_ARS',
+    'pessoa',
+]
